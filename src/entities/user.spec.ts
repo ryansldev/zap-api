4a7d32f9844b913cc97447b8541c0a67d50c7b07
ryansldev <ryanlima1309@gmail.com>
2024-08-19 16:14:35 -0300

    feat: add user entity

---
+++ src/entities/user.spec.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest'
+import { User } from './user'
+
+describe('User', () => {
+  it('should be able to create user', async () => {
+    const user = new User({
+      name: 'John',
+      lastname: 'Doe',
+      email: 'johndoe@gmail.com',
+      password: 'johndoe123',
+      location: 'BR',
+    })
+
+    await user.hashPassword()
+
+    expect(user).toBeInstanceOf(User)
+    expect(user.id).toBeDefined()
+    expect(user.createdAt).toBeDefined()
+    expect(user.updatedAt).toBeDefined()
+    expect(user.password).not.toBe('johndoe123')
+  })
+})
---
+++ src/entities/user.ts
@@ -0,0 +1,97 @@
+import { randomUUID } from 'crypto';
+import { Replace } from '../helpers/Replace'
+import bcrypt from 'bcrypt'
+
+interface UserProps {
+  id: string;
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  profilePic?: string;
+  location: "BR";
+
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+type EntryUserProps = Replace<UserProps, {
+  id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}>;
+
+export class User {
+  private props: UserProps
+
+  constructor(props: EntryUserProps) {
+    const now = new Date()
+    this.props = {
+      id: props.id ?? randomUUID(),
+      ...props,
+      createdAt: props.createdAt ?? now,
+      updatedAt: props.updatedAt ?? now,
+    }
+  }
+
+  get id(): string {
+    return this.props.id
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+  }
+
+  get lastname(): string {
+    return this.props.lastname
+  }
+
+  set lastname(lastname: string) {
+    this.props.lastname = lastname
+  }
+
+  get email(): string {
+    return this.props.email
+  }
+
+  set email(email: string) {
+    this.props.email = email
+  }
+
+  get password(): string {
+    return this.props.password
+  }
+
+  async hashPassword() {
+    const hash = await bcrypt.hash(this.props.password, 10)
+    this.props.password = hash
+  }
+
+  get profilePic(): string | undefined {
+    return this.props.profilePic
+  }
+
+  set profilePic(profilePic: string) {
+    this.props.profilePic = profilePic
+  }
+
+  get location(): "BR" {
+    return this.props.location
+  }
+
+  set location(location: "BR") {
+    this.props.location = location
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt
+  }
+}
