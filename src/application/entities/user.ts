2fb6380b2c2fcdc9cea018ccbeedbe01b5cbcf45
ryansldev <ryanlima1309@gmail.com>
2024-08-19 18:30:37 -0300

    chore: change entities location

--- src/application/entities/user.ts
+++ src/application/entities/user.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'crypto';
-import { Replace } from '../helpers/Replace'
+import { Replace } from '../../helpers/Replace'
 import bcrypt from 'bcrypt'
 import { Post } from './post';
 
