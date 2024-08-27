b9b00cf107555b6bcfb4c5e34c752e2a024c141e
ryansldev <ryanlima1309@gmail.com>
2024-08-27 13:22:56 -0300

    fix: add auth to dislike route

--- src/routes.ts
+++ src/routes.ts
@@ -36,10 +36,12 @@ async function routes (app: FastifyInstance) {
   app.post('/posts/:id/dislike', {
     preHandler: [app.authenticate]
   }, (request, reply) => postsController.dislike(request, reply))
-  app.get('/posts', (request, reply) => postsController.list(request, reply))
+  app.get('/posts', {
+    preHandler: [app.authenticate],
+  }, (request, reply) => postsController.list(request, reply))
   app.get('/posts/:id', (request, reply) => postsController.findById(request, reply))
   app.get('/posts/:id/comments', (request, reply) => postsController.listParents(request, reply))
-  app.get('/posts/:id/comments/count', postsController.countParents)
+  app.get('/posts/:id/comments/count', (request, reply) => postsController.countParents(request, reply))
 }
 
 export default routes
